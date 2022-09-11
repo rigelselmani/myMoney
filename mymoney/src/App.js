9d822522b46ba18a241d72a52c8b55d1c398634a
Rigel <43711248+rigelselmani@users.noreply.github.com>
2022-09-11 14:59:00 -0400

    creating the forms of signup page

--- mymoney/src/App.js
+++ mymoney/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Switch,Route }from "react-router-dom"
+
+//pages
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <BrowserRouter>
+       <Navbar/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/signup">
+          <Signup/>
+        </Route>
+      </Switch>
+     </BrowserRouter>
     </div>
   );
 }
-
-export default App;
+export default App
---
+++ mymoney/src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import styles from "./Home.module.css";
+
+export default function Home(){
+    return(
+        <div>
+            <h1>home</h1>
+        </div>
+    )
+}
